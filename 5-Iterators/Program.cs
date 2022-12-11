e8224bcf356b99099dd110aee9b4855ea3a6675b
Simone Magnani <simone.magnani7@studio.unibo.it>
2022-12-11 15:21:57 +0100

    [5] Add more examples for testing

--- 5-Iterators/Program.cs
+++ 5-Iterators/Program.cs
@@ -51,6 +51,50 @@ namespace Iterators
             {
                 Console.WriteLine(kv);
             }
+
+            Console.WriteLine();
+
+            // Lazy operations won't work unless...
+
+            // ...you use the Enumerator manually...
+            var iterator = Java8StreamOperations.Range(0, 3).GetEnumerator();
+            Console.WriteLine(iterator.Current);
+            while (iterator.MoveNext())
+            {
+                Console.Write($"{iterator.Current},");
+            }
+            Console.WriteLine();
+
+            // ...or a foreach
+            foreach (var item in Java8StreamOperations.Range(0, 5))
+            {
+                Console.Write($"{item},");
+            }
+            Console.WriteLine();
+
+            // ...or a reduce
+            Java8StreamOperations.Range(0, 100)
+                .TakeWhile(n => n < 10)
+                .Peek(n => Console.Write($"{n},"))
+                .Reduce(new Dictionary<int, int>(), (o, n) => { o[n] = 1; return o; });
+            Console.WriteLine();
+
+            // ...or a ForEach
+            Java8StreamOperations.Range(0, 100)
+                .TakeWhile(n => n < 15)
+                .ForEach(n => Console.Write($"{n},"));
+            Console.WriteLine();
+
+            // For eg. this won't display anything
+            Java8StreamOperations.Range(0, 100)
+                .TakeWhile(n => n < 20)
+                .Peek(n => Console.Write($"{n},"));
+            Console.WriteLine();
+            // Equivalent in java: IntStream.range(0, 100).takeWhile(n -> n < 20).peek(n -> System.out.print(n + ","))
+
+            Java8StreamOperations.Range(0, 10)
+                .SkipWhile(n => n < 5)
+                .ForEach(n => Console.Write($"{n},"));
         }
     }
 }
